006a89d6b3df3c93cdde19dcceca87d9ae03b185
Luis150228 <luis.rangeldiaz@gmail.com>
2021-06-09 17:13:23 -0500

    Se agrega card y funcionalidad js

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ tl.to('.slider', {y: '-100%', duration: 1.5, delay: 0.5});
 tl.to('.intro', {y: '-100%', duration: 1}, '-=1');
 tl.fromTo('header', {opacity: 0}, {opacity: 1, duration: 1});
 tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 1.3});
-tl.fromTo('.big-text', {opacity: 0}, {opacity: 1, duration: 1.2}, '-=1');
+// tl.fromTo('.big-text', {opacity: 0}, {opacity: 1, duration: 1.2}, '-=1');
 
 //Control de menu vs secciones
 
@@ -30,6 +30,7 @@ let seccActual = new IntersectionObserver(navCheck, options);
 function navCheck(entries) {
 	entries.forEach((entry) => {
 		const className = entry.target.className;
+		// console.log(className);
 		const activarAncla = document.querySelector(`[data-page=${className}]`);
 		const gradientIndex = entry.target.getAttribute('data-index');
 		const coords = activarAncla.getBoundingClientRect();
@@ -50,4 +51,41 @@ function navCheck(entries) {
 
 secciones.forEach((seccion) => {
 	seccActual.observe(seccion);
+});
+
+///Control de CARDS
+
+const card = document.querySelector('.card');
+const control = document.querySelector('.control');
+
+const titulo = document.querySelector('.title_card');
+const sneaker = document.querySelector('#img1');
+const info = document.querySelector('.info h3');
+
+control.addEventListener('mousemove', (e) => {
+	// console.log(e.pageX, e.pageY);
+	let xAxis = ((window.innerWidth / 2 - e.pageX) / 28) * -1;
+	let yAxis = ((window.innerWidth / 2 - e.pageY) / 50) * -1;
+
+	card.style = 'backdrop-filter: blur(5px)';
+	card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+
+///Animacion de Salida de mouse
+control.addEventListener('mouseenter', (e) => {
+	card.style.transition = 'none';
+	//Contenido up
+	sneaker.style.transform = 'translateZ(210px)';
+	titulo.style.transform = 'translateZ(190px)';
+	info.style.transform = 'translateZ(80px)';
+});
+
+///Animacion de Entrada de mouse
+control.addEventListener('mouseleave', (e) => {
+	card.style.transition = 'all 0.5s ease';
+	card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+	//Contenido Down
+	titulo.style.transform = 'translateZ(0px)';
+	sneaker.style.transform = 'translateZ(0px)';
+	info.style.transform = 'translateZ(0px)';
 });
